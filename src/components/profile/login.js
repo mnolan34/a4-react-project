bf0b6311b41f22b666d2b804bc49a0f46017a4f4
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-26 23:06:01 -0400

    updated heroku url and login

--- src/components/profile/login.js
+++ src/components/profile/login.js
@@ -15,11 +15,13 @@ export const Login = () => {
             <h1>Login</h1>
             <input className="mb-2 form-control"
                    onChange={(e) =>
-                       setLoginUser({...loginUser, username: e.target.value})}
+                       setLoginUser({...loginUser,
+                           username: e.target.value})}
                    placeholder="username"/>
             <input className="mb-2 form-control"
                    onChange={(e) =>
-                       setLoginUser({...loginUser, password: e.target.value})}
+                       setLoginUser({...loginUser,
+                           password: e.target.value})}
                    placeholder="password" type="password"/>
             <button onClick={login}
                     className="btn btn-primary mb-5">Login
