67ee770e2e14473c05322b8044de7bb331b9cbee
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-29 13:01:14 -0400

    Reached end of tuits additions, pushing to test

--- src/components/profile/my-tuits.js
+++ src/components/profile/my-tuits.js
@@ -5,11 +5,15 @@ import Tuits from "../tuits";
 const MyTuits = () => {
     const [tuits, setTuits] = useState([]);
     const findMyTuits = () =>
-        service.findTuitByUser("my")
+        service.findTuitByUser("me")
             .then(tuits => setTuits(tuits));
     useEffect(findMyTuits, []);
+
+    const deleteTuit = (tid) =>
+        service.deleteTuit(tid).then(findMyTuits);
     return(
         <Tuits tuits={tuits}
+               deleteTuit = {deleteTuit}
                refreshTuits={findMyTuits}/>
     );
 };
