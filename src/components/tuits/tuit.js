d1079e02f7bb6a1309459fa7d12af18aac3e992f
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-02 15:30:46 -0400

    Added DislikeTuit to param in tuit/tuit.js

--- src/components/tuits/tuit.js
+++ src/components/tuits/tuit.js
@@ -4,7 +4,7 @@ import TuitImage from "./tuit-image";
 import TuitVideo from "./tuit-video";
 import {useNavigate, Link} from "react-router-dom";
 
-const Tuit = ({tuit, deleteTuit, likeTuit}) => {
+const Tuit = ({tuit, deleteTuit, likeTuit, dislikeTuit}) => {
     const navigate = useNavigate();
     const daysOld = (tuit) => {
         const now = new Date();
